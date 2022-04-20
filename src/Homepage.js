6cc1a1d97e9ea3cc2da3eda1d5be292fc5e0ac4f
KimKeonHee <thsgudfks@naver.com>
2022-04-20 21:47:11 +0900

    컨텐츠 영역 추가 성공

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,30 +1,31 @@
 import logo from './logo.jpg';
+
 import './Homepage.css';
 
 function Homepage() {
   return (
     <div className="Homepage">
-     <div className="menu">
-       <div className='sub-menu'>
-         <div className='menu-button'><a href='https://about.google/?fg=1&utm_source=google-KR&utm_medium=referral&utm_campaign=hp-header'> Google 정보</a></div>
-         <div className='menu-button'><a href='https://store.google.com/KR?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=ko-KR&pli=1'>스토어</a></div>
+      <div className="menu">
+        <div className='sub-menu'>
+          <div className='menu-button'><a href='https://about.google/?fg=1&utm_source=google-KR&utm_medium=referral&utm_campaign=hp-header'> Google 정보</a></div>
+          <div className='menu-button'><a href='https://store.google.com/KR?utm_source=hp_header&utm_medium=google_ooo&utm_campaign=GS100042&hl=ko-KR&pli=1'>스토어</a></div>
         </div>
-       <div className='sub-menu'>
-       <div className='menu-button'><a href='https://mail.google.com/mail/u/0/'>Gmail</a></div>
-       <div className='menu-button'><a href='https://www.google.com'>프로필</a></div>
-       </div>
+        <div className='sub-menu'>
+          <div className='menu-button'><a href='https://mail.google.com/mail/u/0/'>Gmail</a></div>
+          <div className='menu-button'><a href='https://www.google.com'>프로필</a></div>
+        </div>
       </div>
-     <div className='body'>
-       <div className="logo">
-         <a href='https://github.com/free-why-fi'>
-         <img src={logo}/>
-         </a>
+      <div className='body'>
+        <div className="logo">
+          <a href='https://github.com/free-why-fi'>
+            <img src={logo} />
+          </a>
         </div>
-       <div className="search"><input className='search-input'></input></div>
-     </div>
-     
-     
-     <div className="footer">하단</div>
+        <div className="search"><input className='search-input'></input></div>
+      </div>
+
+
+      <div className="footer">하단</div>
     </div>
   );
 }
--- src/SubPage.js
+++ src/SubPage.js
@@ -1,5 +1,9 @@
 import logo from './Logo2.JPG';
 import banner from './banner1.jpg';
+import contentItem1 from './content1.jpg';
+import contentItem2 from './content2.jpg';
+import contentItem3 from './content3.jpg';
+import contentItem4 from './content4.jpg';
 import './SubPage.css';
 
 function SubPage() {
@@ -10,23 +14,51 @@ function SubPage() {
           <a className='menu-item'>회원가입</a>
           <a className='menu-item'>로그인</a>
           <a className='menu-item'>고객센터</a>
-          </div>
+        </div>
         <div className='logo'>
-        <img src={logo}/>
+          <img src={logo} />
         </div>
         <div className='gnb'>
           <a className='menu-item'>전체 카테고리</a>
           <a className='menu-item'>신상품</a>
           <a className='menu-item'>베스트</a>
           <a className='menu-item'>알뜰쇼핑</a>
-          <a className='menu-item'>특가/혜택</a></div>    
+          <a className='menu-item'>특가/혜택</a></div>
       </header>
       <main className='main'>
-      <div className='banner'>
-      <img src={banner}/> 
-      </div>
-      <div className='content'><a>컨텐츠에용</a></div>
-      </main>      
+        <div className='banner'>
+          <img src={banner} />
+        </div>
+        <div className='section'>
+          <div className='title'><a>이 상품 어때요?</a></div>
+          <div className='content'>
+            <a className='content-item'>
+              <img src={contentItem1} />
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='price'>5% 19,000원</p>
+              <p>50,000원</p>
+            </a>
+            <a className='content-item'>
+              <img src={contentItem2} />
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='price'>5% 19,000원</p>
+              <p>50,000원</p>
+            </a>
+            <a className='content-item'>
+              <img src={contentItem3} />
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='price'>5% 19,000원</p>
+              <p>50,000원</p>
+            </a>
+            <a className='content-item'>
+              <img src={contentItem4} />
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='price'>5% 19,000원</p>
+              <p>50,000원</p>
+            </a>
+          </div>
+        </div>
+      </main>
       <footer className='footer'><a>푸터에용</a></footer>
     </div>
 
