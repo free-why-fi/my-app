d3e2eb9a90ec8701966bec8354d7e486a4523515
KimKeonHee <thsgudfks@naver.com>
2022-04-27 21:16:16 +0900

    04-27

--- src/SubPage.js
+++ src/SubPage.js
@@ -1,9 +1,12 @@
 import logo from './Logo2.JPG';
 import banner from './banner1.jpg';
+import banner2 from './banner2.jpg';
 import contentItem1 from './content1.jpg';
 import contentItem2 from './content2.jpg';
 import contentItem3 from './content3.jpg';
 import contentItem4 from './content4.jpg';
+import carticon from './cart_white_45_45.svg';
+
 import './SubPage.css';
 
 function SubPage() {
@@ -19,7 +22,7 @@ function SubPage() {
           <img src={logo} />
         </div>
         <div className='gnb'>
-          <a className='menu-item'>전체 카테고리</a>
+          <a className='menu-item menu-item-red'>전체 카테고리</a>
           <a className='menu-item'>신상품</a>
           <a className='menu-item'>베스트</a>
           <a className='menu-item'>알뜰쇼핑</a>
@@ -33,29 +36,46 @@ function SubPage() {
           <div className='title'><a>이 상품 어때요?</a></div>
           <div className='content'>
             <a className='content-item'>
-              <img src={contentItem1} />
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem1} />
+                <img className='cartimage' src={carticon} />
+              </div>
               <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
-              <p className='price'>5% 19,000원</p>
-              <p>50,000원</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'> 50,000원</p>
             </a>
             <a className='content-item'>
-              <img src={contentItem2} />
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem2} />
+                <img className='cartimage' src={carticon} />
+              </div>
               <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
-              <p className='price'>5% 19,000원</p>
-              <p>50,000원</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'>50,000원</p>
             </a>
             <a className='content-item'>
-              <img src={contentItem3} />
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem3} />
+                <img className='cartimage' src={carticon} />
+              </div>
               <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
-              <p className='price'>5% 19,000원</p>
-              <p>50,000원</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'>50,000원</p>
             </a>
             <a className='content-item'>
-              <img src={contentItem4} />
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem4} />
+                <img className='cartimage' src={carticon} />
+              </div>
               <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
-              <p className='price'>5% 19,000원</p>
-              <p>50,000원</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'>50,000원</p>
             </a>
+
+
+          </div>
+          <div className='banner banner-small'>
+            <img src={banner2} />
           </div>
         </div>
       </main>
