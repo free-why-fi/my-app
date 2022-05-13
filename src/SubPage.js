b97e33318eb8955a2f7b2fe1703a9163f0fcb0f9
KimKeonHee <thsgudfks@naver.com>
2022-05-13 16:44:32 +0900

    05-13 수업

--- src/SubPage.js
+++ src/SubPage.js
@@ -6,6 +6,7 @@ import contentItem2 from './content2.jpg';
 import contentItem3 from './content3.jpg';
 import contentItem4 from './content4.jpg';
 import carticon from './cart_white_45_45.svg';
+import topbutton from './topbutton.png';
 
 import './SubPage.css';
 
@@ -82,7 +83,13 @@ function SubPage() {
               <p className='title'>일일특가</p>
               <p className='description'>24시간 한정특가</p>
               <div className='clock-area'>
-                <div className='clock'>시계</div>
+                <div className='clock'>
+                  <div className='clock-hour'>
+                  </div>
+                  <div className='clock-min'>
+                  </div>
+                </div>
+
                 <p className='time'>14:53:12</p>
               </div>
               <p className='description2'>망설이면 늦어요!</p>
@@ -111,8 +118,60 @@ function SubPage() {
           </div>
 
         </div>
+        <div className='section'>
+          <div className='title'><a>놓치면 후회할 가격</a></div>
+          <div className='content'>
+            <a className='content-item'>
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem1} />
+                <img className='cartimage' src={carticon} />
+              </div>
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'> 50,000원</p>
+            </a>
+            <a className='content-item'>
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem2} />
+                <img className='cartimage' src={carticon} />
+              </div>
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'>50,000원</p>
+            </a>
+            <a className='content-item'>
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem3} />
+                <img className='cartimage' src={carticon} />
+              </div>
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'>50,000원</p>
+            </a>
+            <a className='content-item'>
+              <div className='content-image-area'>
+                <img className='itemimage' src={contentItem4} />
+                <img className='cartimage' src={carticon} />
+              </div>
+              <p className='itemname'>[미램식품] 천연과즙 100% 신선한 아침</p>
+              <p className='display-price'><span className='discount-rate'>5%</span> 19,000원</p>
+              <p className='price'>50,000원</p>
+            </a>
+          </div>
+        </div>
+        <div className='topbutton'>
+          <img className='topbuttonimage' src={topbutton} />
+        </div>
       </main>
-      <footer className='footer'><a>푸터에용</a></footer>
+      <footer className='footer'>
+        <div className='footer-description'>
+          <a>마켓컬리에서 판매되는 상품 중에는 마켓컬리에 입점한 개별 판매자가 판매하는 마켓플레이스(오픈마켓) 상품이 포함되어 있습니다.
+            <p>마켓플레이스(오픈마켓) 상품의 경우 컬리는 통신판매중개자로서 통신판매의 당사자가 아닙니다. 컬리는 해당 상품의 주문, 품질, 교환/환불 등 의무와 책임을 부담하지 않습니다.</p>
+          </a>
+        </div>
+        <div className='right'>© KURLY CORP. ALL RIGHTS RESERVED</div>
+      </footer>
+
     </div>
 
   );
