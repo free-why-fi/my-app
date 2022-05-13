34957c8d5d6ff5719bd881bd68ab40ad44a13713
KimKeonHee <thsgudfks@naver.com>
2022-05-13 17:42:01 +0900

    05-13 수업-2

---
+++ jsmaster.js
@@ -0,0 +1,27 @@
+//[typename] [variablename] = [value]
+// int a = 3;
+//선언
+let a = 3; //변수
+const b = "hi MY NAME IS MIREMI"; //상수
+//console.log(a) //3
+a = 4;
+//b = 4; //error
+// 타입
+// 숫자 정수/실수, 문자, 문자열, 객체 , 함수, 배열, 인스턴스.a....
+
+// 3, // 숫자
+//     "안녕?",// 문자열
+//     { KONI: "BABO" }, //object
+//     [{ KONI: "BABO" }, { KONI: "BABO" }, { KONI: "BABO" }, {}, {}] // 객체 배열
+//     [1, 2, 3, 4] // 숫자배열
+
+//console.log(a) //4
+
+
+
+//// 여기까지 기초!
+
+
+let c = 0;
+let d = 10;
+console.log(c + d)
